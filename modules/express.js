5be9701f098e67c69a8f30e0f40a716b84a7553f
tarcisio.maciel.dev@outlook.com <tarcisio.maciel.dev@outlook.com>
2022-09-29 20:09:58 -0300

    insert ejs components, output users in html page

--- modules/express.js
+++ modules/express.js
@@ -5,8 +5,25 @@ const app = express();
 
 app.use(express.json());
 
+app.set("view engine", "ejs");
+app.set("views", "src/views");
+
+app.use((req, res, next) => {
+  console.log(`Request Type: ${req.method}`);
+  console.log(`Content Type: ${req.headers["content-type"]}`);
+  console.log(`Date: ${new Date()}`);
+
+  next();
+});
+
 app.get("/home", (req, res) => {
   res.status(200).send("<h1>Welcome to my home page</h1>");
+});
+
+app.get("/views/users", async (req, res) => {
+  const users = await UserModel.find({});
+
+  res.render("index", { users });
 });
 
 app.get("/users", async (req, res) => {
